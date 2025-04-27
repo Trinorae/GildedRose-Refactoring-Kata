b67a3e5eb394e0e0284e1e3b9e2e538a30030ce7
Stef Klaassen <stef.klaassen@gmail.com>
2025-04-27 16:58:31 +0200

    Label behaviour in existing code for easier understanding which parts are needed for which type

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,19 +7,30 @@ class GildedRose {
         this.items = items;
     }
 
+    // Item types:
+    // 1. Normal item
+    // 2. Aged Brie
+    // 3. Sulfuras, Hand of Ragnaros
+    // 4. Backstage passes to a TAFKAL80ETC concert
+    // 5. Conjured items (to be added)
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            // Not Aged Brie, not Backstage Passes -> Normal item
             if (!items[i].name.equals("Aged Brie")
                 && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
+                    // And not Sulfuras -> Normal item only
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
+            // Only for Aged Brie and Backstage Passes
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
+                    // Extra for Backstage Passes
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
@@ -36,6 +47,7 @@ class GildedRose {
                 }
             }
 
+            // Only for Normal items, Aged Brie and Backstage Passes -> sellIn of Sulfuras doesn't change ever!
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
@@ -43,14 +55,18 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // Normal Item (and Sulfuras)
                         if (items[i].quality > 0) {
+                            // Not Sulfuras, so just Normal item
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
+                    // Backstage Passes
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
+                // Aged Brie
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
